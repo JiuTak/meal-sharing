ee83fbf7a307c2df0398781173ea43ad7e7b36ca
Jiu Tak <aurajiu@gmail.com>
2021-11-06 15:24:46 +0100

    test

--- src/backend/app.js
+++ src/backend/app.js
@@ -7,7 +7,7 @@ const mealsRouter = require("./api/meals");
 const reservationsRouter = require("./api/reservations");
 const reviewsRouter = require("./api/reviews");
 
-const buildPath = path.join(__dirname, "../../public");
+const buildPath = path.join(__dirname, "../../dist");
 const port = process.env.PORT || 3000;
 const cors = require("cors");
 
