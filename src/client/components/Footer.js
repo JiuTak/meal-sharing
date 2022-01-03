5bd1bd53534eaac3e13f134ba54be71701768043
Jiu Tak <aurajiu@gmail.com>
2022-01-03 21:12:40 +0100

    fixed few errors

--- src/client/components/Footer.js
+++ src/client/components/Footer.js
@@ -6,82 +6,105 @@ export default function Footer() {
     <Container>
       <ul className="footer-container">
         <li>
-          <div className="footer-content">
-            <div className="">
-                <h4>Meal Sharing</h4>
-                <p>Enghavevej 80,</p>
-                <p>2450 København</p>
-            </div>
-          </div>
+          <ul className="footer-content">
+            <h3>Meal Sharing</h3>
+            <p>Enghavevej 80,</p>
+            <p>2450 København</p>
+          </ul>
         </li>
 
         <li>
-          <div className="footer-content">
-            <ul className="footer-contact">                
-            <h4>Contact</h4>
-              <li>
-                <a href="tel:004561608800">+45 00 11 22 33</a>
-              </li>
-              <li>
-                <a href="mailto:info@itkartellet.dk">info@mealsharing.dk</a>
-              </li>
-              <li>
-                <a href="/support">Customer Support</a>
-              </li>
-            </ul>
-          </div>
+          <ul className="footer-content">
+            <h3>Contact</h3>
+            <li>
+              <a href="tel:004561608800">+45 00 11 22 33</a>
+            </li>
+            <li>
+              <a href="mailto:info@itkartellet.dk">info@mealsharing.dk</a>
+            </li>
+            <li>
+              <a href="/support">Customer Support</a>
+            </li>
+          </ul>
         </li>
 
         <li>
-          <div className="footer-content">
-            <h4>Services</h4>
-                <ul class="links-wrapper" style={{ padding:"unset"}}>
-                    <li><a>About</a></li>
-                    <li><a>Meals</a></li>
-                    <li><a>Sharing</a></li>
-                </ul>
-          </div>
+          <ul className="footer-content">
+            <h3>Services</h3>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/meals">Meals</a>
+            </li>
+            <li>
+              <a href="/sharing">Sharing</a>
+            </li>
+          </ul>
         </li>
-        
       </ul>
     </Container>
   );
 }
 
 const Container = styled.div`
+  list-style-type: none;
+  height: 100%;
+  width: 100vw;
+  margin: unset;
+
+  h3 {
+    color: #138b87;
+  }
+
+  a {
+    text-decoration: unset;
+    color: white;
+    line-height: 2rem;
+    transition: all 0.6s linear;
+
+    &:hover {
+      cursor: pointer;
+      color: #49945E;
+      transition: all 0.3 ease;
+      text-decoration: overline;
+    }
+  }
+
+  p {
+    text-align: left;
+    width: 210px;
+  }
+
+  li {
     list-style-type: none;
-    height: 100%;
-    width: 100vw;
+    width: 210px;
+  }
+
+  .footer-container {
+    display: flex;
+    justify-content: space-around;
+    align-items: flex-start;
+    background-color: #b1d8bb;
+    color: white;
+    list-style-type: none;
+    height: 250px;
     margin: unset;
 
-    a {
-        text-decoration: unset;
-        color: white;    
-        line-height: 2rem;    
+    @media (max-width: 960px) {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: space-between;
+      height: 100%;
     }
+  }
 
-    li {
-        list-style-type: none;
-    }
-
-    .footer-container {
-        display: flex;
-        justify-content: space-around;
-        align-items: center;
-        background-color: #b1d8bb;
-        color: white;
-        list-style-type: none;
-        max-width: 1300px;
-        height: 250px;
-        margin: unset;
-
-    }
-
-    .footer-content {
-        align-items: center;
-        flex-direction: column;
-        justify-content: flex-start;
-        align-content: flex-start;
-
-    }
+  .footer-content {
+    align-items: center;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-content: flex-start;
+    padding: unset;
+  }
 `;
